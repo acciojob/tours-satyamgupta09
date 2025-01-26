187bb064a6d5cc8de3a624bc67a02a6c6925fb99
satyamgupta09 <138917027+satyamgupta09@users.noreply.github.com>
2025-01-27 04:31:32 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,13 @@
 import React from "react";
+import './../styles/App.css';
+import Tours from "./Tours";
 
 const App = () => {
-    return(
-      <main id="main">
-      </main>
-    )
-}
+  return (
+    <main id="main">
+      <h1>Tours</h1>
+      <Tours />
+    </main>
+  );
+};
 export default App;
